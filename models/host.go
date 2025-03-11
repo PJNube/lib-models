125bc5c027534d825563c19a446274bfeeadcd77
enju <enjuthulung@gmail.com>
2025-03-11 16:27:02 +0545

    Add IFNULL expression on idx_name_folder_uuid index

--- models/host.go
+++ models/host.go
@@ -9,7 +9,7 @@ type Host struct {
 	UUID       string  `json:"uuid" gorm:"type:varchar(255);unique;primaryKey"`
 	Name       string  `json:"name" gorm:"type:varchar(255);not null;uniqueIndex:idx_name_folder_uuid"`
 	GlobalUUID string  `json:"globalUuid" gorm:"type:varchar(255);not null"`
-	FolderUUID *string `json:"folderUuid" gorm:"type:varchar(255);default:null;uniqueIndex:idx_name_folder_uuid"`
+	FolderUUID *string `json:"folderUuid" gorm:"type:varchar(255);default:null;uniqueIndex:idx_name_folder_uuid,expression:IFNULL(folder_uuid\\,\"\")"`
 }
 
 func (f *Host) BeforeCreate(tx *gorm.DB) (err error) {
