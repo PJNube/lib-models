816b080fd9f1d90dd7c0760ca56e860fb25d0453
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2025-05-01 13:48:02 +0545

    Update gin query params

--- qparams/qparams.go
+++ qparams/qparams.go
@@ -2,39 +2,66 @@ package qparams
 
 import (
 	"errors"
+	"fmt"
+	"net/url"
+	"reflect"
 	"strconv"
 )
 
-type Params map[string][]string
+type Params map[string]any
 
 func (p Params) Get(key string) string {
-	vs := p[key]
-	if len(vs) == 0 {
+	vs, ok := p[key]
+	if !ok {
 		return ""
 	}
-	return vs[0]
+	if !isString(vs) {
+		return ""
+	} else if isString(vs) {
+		return vs.(string)
+	}
+
+	return ""
 }
 
 func (p Params) GetBool(key string) bool {
-	vs := p[key]
-	if len(vs) == 0 {
+	vs, ok := p[key]
+	if !ok {
 		return false
 	}
-	parseBool, _ := strconv.ParseBool(vs[0])
-	return parseBool
+	isString := isString(vs)
+	if !isString {
+		return false
+	} else if isString {
+		boolVal, err := strconv.ParseBool(vs.(string))
+		if err != nil {
+			return false
+		}
+		return boolVal
+	}
+	return false
 }
 
 func (p Params) GetInt(key string) int {
-	vs := p[key]
-	if len(vs) == 0 {
+	vs, ok := p[key]
+	if !ok {
 		return 0
 	}
-	intValue, _ := strconv.Atoi(vs[0])
-	return intValue
+	isString := isString(vs)
+	if !isString {
+		return 0
+	} else if isString {
+		intVal, err := strconv.Atoi(vs.(string))
+		if err != nil {
+			return 0
+		}
+		return intVal
+	}
+	return 0
 }
 
-func (p Params) Set(key, value string) {
-	p[key] = []string{value}
+func (p Params) Set(key string, value any) {
+	p[key] = value
 }
 
 func (p Params) Del(key string) {
@@ -51,4 +78,44 @@ func (p Params) Validate(key string) error {
 		return errors.New(key + " is required")
 	}
 	return nil
+}
+
+func isString(value any) bool {
+	if reflect.TypeOf(value).Kind() == reflect.Slice {
+		return false
+	} else if reflect.TypeOf(value).Kind() == reflect.String {
+		return true
+	}
+	return false
+}
+
+func GetParams(params url.Values) Params {
+	result := make(Params)
+	for key, value := range params {
+		if len(value) == 1 {
+			result[key] = value[0]
+		} else {
+			result[key] = value
+		}
+	}
+	return result
+}
+
+func (p Params) RefreshParams() Params {
+	var resultMap Params = make(Params)
+	for k, v := range p {
+		vT := reflect.TypeOf(v).Kind()
+		if vT == reflect.Float32 || vT == reflect.Float64 || vT == reflect.Int || vT == reflect.Int32 || vT == reflect.Int64 || vT == reflect.Bool {
+			resultMap[k] = fmt.Sprintf("%v", v)
+		} else if vT == reflect.Slice || vT == reflect.Array {
+			var listVal []string
+			for _, val := range v.([]any) {
+				listVal = append(listVal, fmt.Sprintf("%v", val))
+			}
+			resultMap[k] = listVal
+		} else {
+			resultMap[k] = v
+		}
+	}
+	return resultMap
 }
