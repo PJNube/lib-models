a2b9750deb177762c6d4c4c2a450877eeb55052a
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2025-05-15 15:51:44 +0545

    feat: Update query-params

--- qparams/qparams.go
+++ qparams/qparams.go
@@ -1,95 +1,30 @@
 package qparams
 
 import (
-	"errors"
+	"encoding/json"
 	"net/url"
-	"reflect"
-	"strconv"
 )
 
-type Params map[string]any
-
-func (p Params) Get(key string) string {
-	vs, ok := p[key]
-	if !ok {
-		return ""
-	}
-	if !isString(vs) {
-		return ""
-	} else {
-		return vs.(string)
-	}
+type Params struct {
+	Name              string `json:"name"`
+	Depth             int    `json:"depth"`
+	Address           string `json:"address"`
+	ForceUpload       bool   `json:"force_upload"`
+	DownloadExtension bool   `json:"download_extension"`
+	Architecture      string `json:"architecture"`
+	ParentUuid        string `json:"parent_uuid"`
+	WithNesting       bool   `json:"with_nesting"`
+	WithHosts         bool   `json:"with_hosts"`
+	WithVersions      bool   `json:"with_versions"`
+	WithData          bool   `json:"with_data"`
+	WithDashboard     bool   `json:"with_dashboard"`
 }
 
-func (p Params) GetBool(key string) bool {
-	vs, ok := p[key]
-	if !ok {
-		return false
+func GetParam(params url.Values) (*Params, error) {
+	result := new(Params)
+	err := json.Unmarshal([]byte(params["params"][0]), result)
+	if err != nil {
+		return nil, err
 	}
-	if !isString(vs) {
-		return false
-	} else {
-		boolVal, err := strconv.ParseBool(vs.(string))
-		if err != nil {
-			return false
-		}
-		return boolVal
-	}
-}
-
-func (p Params) GetInt(key string) int {
-	vs, ok := p[key]
-	if !ok {
-		return 0
-	}
-	if !isString(vs) {
-		return 0
-	} else {
-		intVal, err := strconv.Atoi(vs.(string))
-		if err != nil {
-			return 0
-		}
-		return intVal
-	}
-}
-
-func (p Params) Set(key string, value any) {
-	p[key] = value
-}
-
-func (p Params) Del(key string) {
-	delete(p, key)
-}
-
-func (p Params) Has(key string) bool {
-	_, ok := p[key]
-	return ok
-}
-
-func (p Params) Validate(key string) error {
-	if !p.Has(key) {
-		return errors.New(key + " is required")
-	}
-	return nil
-}
-
-func isString(value any) bool {
-	if reflect.TypeOf(value).Kind() == reflect.Slice {
-		return false
-	} else if reflect.TypeOf(value).Kind() == reflect.String {
-		return true
-	}
-	return false
-}
-
-func GetParams(params url.Values) Params {
-	result := make(Params)
-	for key, value := range params {
-		if len(value) == 1 {
-			result[key] = value[0]
-		} else {
-			result[key] = value
-		}
-	}
-	return result
+	return result, nil
 }
