240b8c7980f4f8252465b4287cac97353256fb46
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2025-08-14 13:14:32 +0545

    Update Role dto

--- qparams/permissions.go
+++ qparams/permissions.go
@@ -1,12 +1,1 @@
 package qparams
-
-type FeatureParam struct {
-	ExtensionId string    `json:"extensionId"`
-	Username    string    `json:"username"`
-	Features    []Feature `json:"features"`
-}
-
-type Feature struct {
-	Feature string   `json:"feature"`
-	Action  []string `json:"action"`
-}
