7d60499784ba08c2cb2fdb52d1feac5e68d21681
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2025-09-26 08:00:04 +0545

    Update permissions dtos

--- dtos/permissions.go
+++ dtos/permissions.go
@@ -1,21 +1,24 @@
 package dtos
 
 type Permission struct {
-	Resource    string `json:"resource"`
-	Action      string `json:"action"`
-	ExtensionId string `json:"extensionId"`
+	Item          string `json:"item"` // resource
+	Action        string `json:"action"`
+	ExtensionName string `json:"extensionId"`
 }
 
 type Permissions struct {
-	Resources []*Permission `json:"resources"`
+	Resources []*Permission `json:"items"`
 }
 
 type FeatureResource struct {
-	ID        string   `json:"id"`
-	Resources []string `json:"resources,omitempty"`
+	ID          string   `json:"id"`
+	Description string   `json:"description,omitempty"`
+	Items       []string `json:"items,omitempty"` // resources
 }
 
 type ResourceParam struct {
-	Ids         []string `json:"ids"`
-	ExtensionId string   `json:"extensionId"`
+	Ids          []string `json:"ids"`
+	ExtensionId  string   `json:"extensionId"`  // now full id with profile, vendor and name for casbin
+	Version      string   `json:"version"`      // v1.0.0
+	Architecture string   `json:"architecture"` // arm64, amd64
 }
