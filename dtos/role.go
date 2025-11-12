80a8ad3308db0ffdd0cea63cb9628499f09862c0
enju <enjuthulung@gmail.com>
2025-11-12 10:15:38 +0545

    Minor improvements

--- dtos/role.go
+++ dtos/role.go
@@ -1,14 +1,15 @@
 package dtos
 
 type Role struct { // role create and upsert
-	Name        string      `json:"name"`
-	ExtensionID string      `json:"extensionId,omitempty"`
-	Resources   []*Resource `json:"resources,omitempty"`
+	Name      string      `json:"name"`
+	Action    string      `json:"action,omitempty"`
+	Resources []*Resource `json:"resources,omitempty"`
 }
 
 type Resource struct {
-	ID     string `json:"id"`
-	Action string `json:"action,omitempty"`
+	ExtensionID string `json:"extensionId,omitempty"`
+	ID          string `json:"id"`
+	Action      string `json:"action,omitempty"`
 }
 
 type CasbinPolicy struct {
