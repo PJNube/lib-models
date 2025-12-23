3e87d489c9f08cf1ce4e3b7060b0db2605da5237
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2025-12-23 14:53:45 +0545

    Add version lock for db restore

--- dtos/backup.go
+++ dtos/backup.go
@@ -24,3 +24,9 @@ func (c *CreateBackupRequest) ValidateComponents() (bool, error) {
 type DeleteBackupsRequest struct {
 	UUIDs []string `json:"uuids"`
 }
+
+type ExtensionVersionLock struct {
+	Version     string `json:"version"`
+	VersionPath string `json:"version_path"`
+	DataPath    string `json:"data_path"`
+}
