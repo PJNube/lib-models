c86318c885b8a9437a1a1faacc517ab587cd0bc7
MarkMaYZ <mark.ma@pjoshua.onmicrosoft.com>
2026-01-15 10:26:28 +0800

    modify users dto

--- dtos/user.go
+++ dtos/user.go
@@ -8,7 +8,7 @@ import (
 type User struct {
 	UUID           string               `json:"uuid"`
 	Username       string               `json:"username"`
-	PasswordExpiry int64                `json:"passwordExpiry"` // Unix timestamp in seconds
+	PasswordExpiry *int64               `json:"passwordExpiry"` // Unix timestamp in seconds
 	Status         datatypes.UserStatus `json:"status"`
 	Layouts        []*models.UserLayout `json:"layouts"`
 
