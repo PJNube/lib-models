5d8e66da19e04920afdcaa1c5e1c114bb2995d14
MarkMaYZ <mark.ma@pjoshua.onmicrosoft.com>
2026-02-24 15:48:38 +0800

    Return const uppercase error msg pjn924 (#29)

--- utils/api/api.go
+++ utils/api/api.go
@@ -41,12 +41,13 @@ func BuildPaginatedResponse(data any, total int64, page int, size int) *dtos.API
 	}
 }
 
-func BuildForbiddenAPIResponse(data any, message string) *dtos.APIResponse {
+func BuildErrorResponse(code int, errCode, message string) *dtos.APIResponse {
 	return &dtos.APIResponse{
 		Status:  "error",
-		Code:    403,
+		Code:    code,
 		Message: &message,
-		Data:    data,
+		ErrCode: errCode,
+		Data:    nil,
 	}
 }
 
