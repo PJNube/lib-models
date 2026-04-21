3b4e495b3c65e1434630eb20b6c7d3d94985400f
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2026-04-21 21:58:15 +0545

    Update audit-logs model

--- enums/auditlog.go
+++
@@ -1,8 +0,0 @@
-package enums
-
-type OperationType string
-
-const (
-	OperationWrite    OperationType = "WRITE"
-	OperationOverride OperationType = "OVERRIDE"
-)
--- models/auditlog.go
+++ models/auditlog.go
@@ -3,19 +3,16 @@ package models
 import (
 	"time"
 
-	"github.com/PJNube/lib-models/enums"
 	"gorm.io/datatypes"
 )
 
 type AuditLog struct {
-	ID            uint64              `json:"id" gorm:"primaryKey;autoIncrement"`
-	Username      string              `json:"username,omitempty" gorm:"size:128"`
-	ClientIP      string              `json:"clientIp,omitempty" gorm:"size:45"`
-	Action        string              `json:"action,omitempty" gorm:"not null;size:64"`
-	Url           string              `json:"url,omitempty" gorm:"not null;size:256"`
-	ResourceId    string              `json:"resourceId,omitempty" gorm:"not null"`
-	Body          datatypes.JSON      `json:"body,omitempty" gorm:"type:json"`
-	PreviousBody  datatypes.JSON      `json:"previousBody,omitempty" gorm:"type:json"`
-	CreatedAt     time.Time           `json:"auditTime,omitempty" gorm:"not null;autoCreateTime"`
-	OperationType enums.OperationType `json:"operationType,omitempty"`
+	ID           uint64         `json:"id" gorm:"primaryKey;autoIncrement"`
+	Username     string         `json:"username,omitempty" gorm:"size:128"`
+	ClientIP     string         `json:"clientIp,omitempty" gorm:"size:45"`
+	Action       string         `json:"action,omitempty" gorm:"not null;size:64"`
+	Url          string         `json:"url,omitempty" gorm:"not null;size:256"`
+	Body         datatypes.JSON `json:"body,omitempty" gorm:"type:json"`
+	PreviousBody datatypes.JSON `json:"previousBody,omitempty" gorm:"type:json"`
+	CreatedAt    time.Time      `json:"auditTime,omitempty" gorm:"not null;autoCreateTime"`
 }
