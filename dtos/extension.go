f1cf617576589db7e638a365034aa13113462fcd
MarkMaYZ <mark.ma@pjoshua.onmicrosoft.com>
2026-04-23 18:44:14 +0800

    revert dist version (#38)

--- dtos/extension.go
+++ dtos/extension.go
@@ -3,8 +3,7 @@ package dtos
 import "github.com/PJNube/lib-models/datatypes"
 
 type ExtensionBatch struct {
-	Type        datatypes.ExtensionBatchType `json:"type"`
-	ID          string                       `json:"id"`
-	Version     string                       `json:"version"`
-	DistVersion string                       `json:"distVersion"`
+	Type    datatypes.ExtensionBatchType `json:"type"`
+	ID      string                       `json:"id"`
+	Version string                       `json:"version"`
 }
