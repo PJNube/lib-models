5ca6ea6c73bbb8f23c5116b0baff35c4a8ca9318
abishz17 <075bct008.abish@pcampus.edu.np>
2026-05-19 14:18:54 +0545

    refator:name

--- datatypes/backupcomponent.go
+++ datatypes/backupcomponent.go
@@ -8,27 +8,27 @@ import (
 type BackupTier string
 
 const (
-	BackupComponentControlApp BackupTier = "control-app"
-	BackupComponentData       BackupTier = "data"
-	BackupComponentAll        BackupTier = "all"
+	BackupTierControlApp     BackupTier = "control-app"
+	BackupTierExtensionsData BackupTier = "extensions-data"
+	BackupTierAll            BackupTier = "all"
 )
 
-var BackupComponentMap = map[BackupTier]struct{}{
-	BackupComponentControlApp: {},
-	BackupComponentData:       {},
-	BackupComponentAll:        {},
+var BackupTierMap = map[BackupTier]struct{}{
+	BackupTierControlApp:     {},
+	BackupTierExtensionsData: {},
+	BackupTierAll:            {},
 }
 
 func (dt *BackupTier) Validate() error {
 	if dt == nil || *dt == "" {
 		return nil
 	}
-	if _, ok := BackupComponentMap[*dt]; ok {
+	if _, ok := BackupTierMap[*dt]; ok {
 		return nil
 	}
-	v := make([]string, 0, len(BackupComponentMap))
-	for m := range BackupComponentMap {
+	v := make([]string, 0, len(BackupTierMap))
+	for m := range BackupTierMap {
 		v = append(v, string(m))
 	}
-	return fmt.Errorf("please provide a valid backup component i.e.: %s", strings.Join(v, " or "))
+	return fmt.Errorf("please provide a valid backup tier i.e.: %s", strings.Join(v, " or "))
 }
