b243415a230da469610621ffb0a2ef55520f3db0
Abish Bhusal <52349173+abishz17@users.noreply.github.com>
2026-05-29 08:17:07 +0545

    add restore fields (#43)

--- models/restore.go
+++ models/restore.go
@@ -9,12 +9,15 @@ import (
 )
 
 type Restore struct {
-	UUID       string                   `json:"uuid" sql:"uuid" gorm:"type:varchar(255);unique;primaryKey"`
-	BackupUUID string                   `json:"backupUuid" gorm:"type:varchar(255)"`
-	Status     datatypes.ProgressStatus `json:"status"`
-	Progress   int                      `json:"progress"`
-	Message    string                   `json:"message,omitempty"`
-	CreatedAt  time.Time                `json:"createdAt"`
+	UUID                 string                   `json:"uuid" sql:"uuid" gorm:"type:varchar(255);unique;primaryKey"`
+	BackupUUID           string                   `json:"backupUuid" gorm:"type:varchar(255)"`
+	BackupFileName       string                   `json:"backupFileName,omitempty"`
+	Tier                 string                   `json:"tier,omitempty"`
+	IncludeSystemUpgrade bool                     `json:"includeSystemUpgrade,omitempty"`
+	Status               datatypes.ProgressStatus `json:"status"`
+	Progress             int                      `json:"progress"`
+	Message              string                   `json:"message,omitempty"`
+	CreatedAt            time.Time                `json:"createdAt"`
 }
 
 func (b *Restore) BeforeCreate(tx *gorm.DB) (err error) {
