bda838563cf2743f3c1b649434cbb866f2c3945c
terry-pjo <terry.liao@pjoshua.onmicrosoft.com>
2026-08-04 15:25:30 +0800

    extensionId must be exist and not empty

--- dtos/event.go
+++ dtos/event.go
@@ -1,7 +1,7 @@
 package dtos
 
 type EventPayload struct {
-	ExtensionID string   `json:"extensionId,omitempty"`
+	ExtensionID string   `json:"extensionId"` // extensionId must be exist and not empty
 	Event       string   `json:"event,omitempty"`
 	Payload     any      `json:"payload,omitempty"`
 	Tags        []string `json:"tags,omitempty"`
